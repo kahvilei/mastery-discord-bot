e041d1f75fbbea52450f08573624b1e88a30dfec
kahvilei <luovakatie@gmail.com>
2022-05-30 18:29:25 -0400

    main-content wrappper setup

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import './css/App.css';
 
 function App() {
   return (
-    <div className="summoner-watcher-wrapper">
-      <header className="summoner-watcher-header">
-        <div className="header-content">
-          <h1>Summoner Watch</h1>
-          <nav></nav>
-        </div>
-      </header>
-      <body>
 
-      </body>
-    </div>
+      <div className="summoner-watcher-wrapper">
+        <header className="summoner-watcher-header">
+          <div id="header-content">
+            <h1>Summoner Watch</h1>
+            <nav>
+                <a>Home</a>
+                <a>Leaderboards</a>
+                <a>Summoner</a>
+            </nav>
+          </div>
+        </header>
+        <section id="main-content">
+
+        </section>
+      </div>
+
   );
 }
 
