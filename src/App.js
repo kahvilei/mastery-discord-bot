27548a4fb8efe771e2120992a73f060ffd5a0410
kahvilei <luovakatie@gmail.com>
2022-05-30 17:32:44 -0400

    header updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="summoner-watcher-wrapper">
       <header className="summoner-watcher-header">
-        <h1>Summoner Watch</h1>
+        <div className="header-content">
+          <h1>Summoner Watch</h1>
+          <nav></nav>
+        </div>
       </header>
       <body>
 
