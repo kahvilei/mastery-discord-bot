785611f6d454b5b91398dec0bebe085f13001a1d
kahvilei <luovakatie@gmail.com>
2022-05-30 23:15:38 -0400

    summoners added to main page using temp json file

---
+++ src/assets/js/summonerData.js
@@ -0,0 +1,38 @@
+export const summonerData = [
+    {
+        accountId: "vP67_tc53x4VczBjSByYeS-aoEGKEDB7C7H41cUGFb5CxVY",
+        id: "myxx_roMhbbHdfTdiNFpXgLRY9wjmFkyZu6hDfdqP5e-psk",
+        name: "Kadie",
+        profileIconId: "5257",
+        puuid: "t2rDGLJnK1wGoXwRUoh3FCHM-UfaCTIi0F3hKQde6lkzK9Pjujpni0RW-8vwnyv_rF_k1tdtzioHxw",
+        revisionDate: "1653712677000",
+        summonerLevel: "124"
+    },
+    {
+        accountId: "vP67_tc53x4VczBjSByYeS-aoEGKEDB7C7H41cUGFb5CxVY",
+        id: "myxx_roMhbbHdfTdiNFpXgLRY9wjmFkyZu6hDfdqP5e-psk",
+        name: "Kadie",
+        profileIconId: "5257",
+        puuid: "t2rDGLJnK1wGoXwRUoh3FCHM-UfaCTIi0F3hKQde6lkzK9Pjujpni0RW-8vwnyv_rF_k1tdtzioHxw",
+        revisionDate: "1653712677000",
+        summonerLevel: "124"
+    },
+    {
+        accountId: "vP67_tc53x4VczBjSByYeS-aoEGKEDB7C7H41cUGFb5CxVY",
+        id: "myxx_roMhbbHdfTdiNFpXgLRY9wjmFkyZu6hDfdqP5e-psk",
+        name: "Kadie",
+        profileIconId: "5257",
+        puuid: "t2rDGLJnK1wGoXwRUoh3FCHM-UfaCTIi0F3hKQde6lkzK9Pjujpni0RW-8vwnyv_rF_k1tdtzioHxw",
+        revisionDate: "1653712677000",
+        summonerLevel: "124"
+    },
+    {
+        accountId: "vP67_tc53x4VczBjSByYeS-aoEGKEDB7C7H41cUGFb5CxVY",
+        id: "myxx_roMhbbHdfTdiNFpXgLRY9wjmFkyZu6hDfdqP5e-psk",
+        name: "Kadie",
+        profileIconId: "5257",
+        puuid: "t2rDGLJnK1wGoXwRUoh3FCHM-UfaCTIi0F3hKQde6lkzK9Pjujpni0RW-8vwnyv_rF_k1tdtzioHxw",
+        revisionDate: "1653712677000",
+        summonerLevel: "124"
+    }
+  ];
--- src/components/Summoners.js
+++ src/components/Summoners.js
@@ -1,7 +1,28 @@
+import {summonerData} from '../assets/js/summonerData.js'
+
 function Summoners() {
     return (
         <section id="summoner-previews">
             <h2>Summoners</h2> 
+            <div className="summoner-preview-list">
+                {summonerData.map((data, key) => {
+                return (
+                    <div className="summoner-container" key={key}>
+                        <div className = "summoner-icon"> 
+                            <img src = {"http://ddragon.leagueoflegends.com/cdn/12.10.1/img/profileicon/" + data.profileIconId + ".png"}></img>
+                        </div>
+                        <div className = "summoner-info">
+                            <h3 className = "summoner-name">
+                                {data.name}
+                            </h3>
+                            <div className = "summoner-level">
+                                Level {data.summonerLevel}
+                            </div>
+                        </div>
+                    </div>
+                );
+                })}
+            </div>
         </section>
     );
   }
