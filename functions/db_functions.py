ca4e41c5cfc2983fffbbdcb3d05486c2555c31f9
Sam <sambritton212@gmail.com>
2022-06-19 16:06:55 -0400

    Start of winrate calc

--- functions/db_functions.py
+++ functions/db_functions.py
@@ -1,3 +1,5 @@
+from time import time
+
 from google.cloud import datastore
 import json
 import flask
@@ -55,6 +57,34 @@ def delete_user(datastore_client, request_args):
         db_key = datastore_client.key("summoner", puuid)
         summoner = datastore_client.get(key=db_key)
         datastore_client.delete(summoner)
-        return "summoner sucessfully deleted"
+        return "summoner successfully deleted"
     except KeyError:
         return None
+
+
+def update_user_winrate(datastore_client, request_args):
+
+    if "puuid" not in request_args:
+        return "puuid required for updating user matches"
+    puuid = request_args["puuid"]
+
+    last_updated = time()
+    query = datastore_client.query(kind="user_winrate")
+    query.add_filter("puuid", "=", puuid)
+    query_result = list(query.fetch())
+    user_winrate = json.loads(json.dumps(query_result), parse_int=str)
+    if len(user_winrate) > 0:
+        # TODO this is probably wrong
+        last_updated = user_winrate[0]["last_added"]
+
+    query = datastore_client.query(kind="summoner_match")
+    query.add_filter("gameStartTimestamp", ">", last_updated)
+    query_result = list(query.fetch())
+    new_matches = json.loads(json.dumps(query_result), parse_int=str)
+
+    wins = len([match for match in new_matches if match["win"]])
+    new_match_count = len(new_matches)
+
+    # TODO update the user_winrate table
+
+    # TODO update the summoner table
--- functions/testing.py
+++ functions/testing.py
@@ -8,9 +8,10 @@ request = flask.Request({'request': 'test'})
 try:
     request.args = {'operation': sys.argv[1]}
     if sys.argv[1] == 'delete_user':
-        puuid = input("Please enter the puuid of the summoner you would like to delete: ")
-        request.args = {"puuid": puuid, "operation": 'delete_user'}
-    if sys.argv[1] == 'add_user':
+        request.args = {"puuid": sys.argv[2], "operation": 'delete_user'}
+    elif sys.argv[1] == 'update_user_winrate':
+        request.args = {"puuid": sys.argv[2], "operation": 'update_user_winrate'}
+    elif sys.argv[1] == 'add_user':
         summoner = input("Please enter the name of the summoner you would like to add: ")
         region = input("And the region (will default to na1 on incorrect input): ")
         request.args = {'summoner': summoner, 'region': region, 'operation': 'add_user'}
